95f631048806fefc6f2065924ef4fcddda18f9a9
Lukas Humbel <lukas.humbel@inf.ethz.ch>
2019-06-18 16:04:21 +0200

    sockeye: Start monadized prolog backend
    Signed-off-by: Lukas Humbel <lukas.humbel@inf.ethz.ch>

--- SockeyeBackendProlog.hs
+++ SockeyeBackendProlog.hs
@@ -33,6 +33,7 @@ import Data.Char
 import Data.List
 import Text.Printf
 import Control.Exception (throw, Exception)
+import Control.Monad.State.Strict
 
 import qualified SockeyeSymbolTable as ST
 import qualified SockeyeAST as SAST
@@ -49,19 +50,65 @@ compile :: ST.Sockeye -> SAST.Sockeye -> String
 compile symTable ast = "Prolog backend not yet implemented"
 
 compileDirect :: AST.Sockeye -> String
-compileDirect = generate
+compileDirect = generate_sockeye
 
-{- Code Generator -}
-class PrologGenerator a where
-    generate :: a -> String
 
-instance PrologGenerator AST.Sockeye where
-  generate s = let
+{- The Module level state (State counter, temp variable counter etc) and the
+ - corresponding Monad
+ -}
+data ModLevelState = ModLevelState { tmpCount :: Int
+                                   , stateCount :: Int
+                                   , extraPred :: [String]
+                                   }
+
+type ModLevelStateS = State ModLevelState 
+
+init_mod_state :: ModLevelState 
+init_mod_state = ModLevelState {tmpCount = 0, stateCount = 0, extraPred = []}
+
+{- get a new (per-module) unique temporary variable -}
+get_next_tmp_var :: ModLevelStateS String
+get_next_tmp_var = do
+    st <- get
+    modify (\c -> c { tmpCount = (tmpCount c) + 1})
+    return $ "TMP_" ++ (show $ tmpCount st)
+
+get_state_var :: ModLevelStateS String
+get_state_var = do
+    st <- get
+    return $ statevar (stateCount st)
+
+get_next_state_var :: ModLevelStateS String
+get_next_state_var = do
+    st <- get
+    modify (\c -> c { stateCount = (stateCount c) + 1})
+    return $ statevar (stateCount st)
+
+{- add an extra predicate to be inserted at the module level -}
+push_extra_pred :: String -> ModLevelStateS ()
+push_extra_pred pred = do
+    st <- get
+    modify (\c -> c { extraPred = (pred : (extraPred c))})
+    return ()
+
+
+{- Code Generator, first part, outside the module context -}
+generate_sockeye :: AST.Sockeye -> String
+generate_sockeye s = let
     files = map snd (Map.toList (AST.files s))
-    in concat (map generate files)
+    in concat (map generate_sockeye_file files)
 
-instance PrologGenerator AST.SockeyeFile where
-  generate f = concat (map generate (AST.modules f))
+generate_sockeye_file :: AST.SockeyeFile -> String
+generate_sockeye_file f = 
+    let
+        -- gmod :: AST.module -> String
+        gmod m = fst $ runState (generate m) init_mod_state
+    in 
+        concat (map gmod (AST.modules f))
+
+
+class PrologGenerator a where
+    generate :: a -> ModLevelStateS String
 
 gen_node_param_list :: [AST.NodeDeclaration] -> [String]
 gen_node_param_list ndl = map AST.nodeName ndl
@@ -77,33 +124,43 @@ gen_body_def_maps m (xs:x) i = elms  ++ (gen_body_def_maps m x (ii))
         (ii, elms) = (gen_body_defs m xs i)
 
 instance PrologGenerator AST.Module where
-  generate m = let
-    name = "add_" ++ AST.moduleName m
-    mi = gen_module_info m
-    p1 = gen_nat_param_list (AST.parameters m)
-    bodyChecks = [predicate "is_list" ["Id"]] ++ map (\x -> predicate "nonvar" [x]) p1
+  generate m = do
+    let name = "add_" ++ AST.moduleName m
+    let mi = gen_module_info m
+    let p1 = gen_nat_param_list (AST.parameters m)
+    let bodyChecks = [predicate "is_list" ["Id"]] ++ map (\x -> predicate "nonvar" [x]) p1
 
-    nodeDecls = concat $ map gen_node_decls (AST.nodeDecls m)
-    instDecls = map gen_inst_decls (AST.instDecls m)
+    nodeDecls <- mapM generate (AST.nodeDecls m)
+    instDecls <- mapM gen_inst_decls (AST.instDecls m)
 
     -- bodyDefs = concat $ map (gen_body_defs mi) (AST.definitions m)
-    bodyDefs = gen_body_def_maps mi  (AST.definitions m) 0
-    n =  sum $ map (count_num_facts mi) (AST.definitions m)
+    -- let bodyDefs = gen_body_def_maps mi  (AST.definitions m) 0
+    let bodyDefs =  []
+    -- n =  sum $ map (count_num_facts mi) (AST.definitions m)
 
-    body = intercalate ",\n    " $ bodyChecks ++ nodeDecls ++ instDecls ++ bodyDefs
-    in name ++ stringify ([statevar 0, "Id"] ++ p1 ++ [statevar n]) ++ " :- \n    " ++ body ++ ".\n\n"
+    let body = intercalate ",\n    " $ bodyChecks ++ nodeDecls ++ instDecls ++ bodyDefs
+    cS <- get_state_var
+    return $ name ++ stringify ([statevar 0, "Id"] ++ p1 ++ [cS]) ++ " :- \n    " ++ body ++ ".\n\n"
     where
       stringify [] = ""
       stringify pp = parens $ intercalate "," pp
 
 
 instance PrologGenerator AST.PropertyExpr where
-  generate (AST.And _ a b) = predicate "and" [generate a, generate b]
-  generate (AST.Or _ a b) = predicate "or" [generate a, generate b]
-  generate (AST.Not _ a) = predicate "not" [generate a]
-  generate (AST.Property _ s) = atom s
-  generate (AST.True) = atom "true"
-  generate (AST.False) = atom "false"
+  generate (AST.And _ a b) = do
+            l <- generate a
+            r <- generate b
+            return $ predicate "and" [l, r]
+  generate (AST.Or _ a b) = do
+            l <- generate a
+            r <- generate b
+            return $ predicate "or" [l, r]
+  generate (AST.Not _ a) = do
+            l <- generate a
+            return $ predicate "not" [l]
+  generate (AST.Property _ s) = return $ atom s
+  generate (AST.True) = return $ atom "true"
+  generate (AST.False) = return $ atom "false"
 
 
 
@@ -129,32 +186,31 @@ local_const_name x = "CONST_" ++ x
 
 -- Generates something a la:
 -- (ID_RAM) = (['ram', Id])
-gen_inst_decls :: AST.InstanceDeclaration -> String
-gen_inst_decls x =
-  let
-    var = local_nodeid_name $ AST.instName x
-    decl = list_prepend (doublequotes $ AST.instName x) ("Id")
-    in var ++ " = " ++ decl
+instance PrologGenerator AST.InstanceDeclaration where
+    generate x =
+      do
+        let var = local_nodeid_name $ AST.instName x
+        let decl = list_prepend (doublequotes $ AST.instName x) ("Id")
+        return $ var ++ " = " ++ decl
 
 -- Generates something a la:
 -- (ID_RAM, INKIND_RAM, OUTKIND_RAM) = (['ram' | Id], memory, memory)
-gen_node_decls :: AST.NodeDeclaration -> [String]
-gen_node_decls x =
-  let
-    var = local_nodeid_name $ AST.nodeName x
-    decl_kind_in = generate (AST.originDomain (AST.nodeType x))
-    decl_kind_out = generate (AST.targetDomain (AST.nodeType x))
-    decl_id = list_prepend (doublequotes $ AST.nodeName x) ("Id")
-    decl_tup = tuple [decl_id, decl_kind_in, decl_kind_out]
+instance PrologGenerator AST.NodeDeclaration where
+    generate x = do
+        let var = local_nodeid_name $ AST.nodeName x
+        decl_kind_in <- generate (AST.originDomain (AST.nodeType x))
+        decl_kind_out <- generate (AST.targetDomain (AST.nodeType x))
+        let decl_id = list_prepend (doublequotes $ AST.nodeName x) ("Id")
+        let decl_tup = tuple [decl_id, decl_kind_in, decl_kind_out]
 
-    -- Build the variable list
-    pf = AST.nodeName x
-    var_tup = tuple [local_nodeid_name pf, "INKIND_" ++ pf, "OUTKIND_" ++ pf]
-    in [
-        var_tup ++ " = " ++ decl_tup,
-        predicate "node_enum" [local_nodeid_name pf, "_"], --eager node enum 
-        predicate "nonvar" ["INKIND_" ++ pf],  -- remove singleton var warning
-        predicate "nonvar" ["OUTKIND_" ++ pf]]
+        -- Build the variable list
+        let pf = AST.nodeName x
+        let var_tup = tuple [local_nodeid_name pf, "INKIND_" ++ pf, "OUTKIND_" ++ pf]
+        return $ intercalate ",\n" [
+            var_tup ++ " = " ++ decl_tup,
+            predicate "node_enum" [local_nodeid_name pf, "_"], --eager node enum 
+            predicate "nonvar" ["INKIND_" ++ pf],  -- remove singleton var warning
+            predicate "nonvar" ["OUTKIND_" ++ pf]]
 
 
 -- This transformation is probably better to be in an AST transform
@@ -164,8 +220,8 @@ data MyAddressBlock = MyAddressBlock {
   properties    :: AST.PropertyExpr
   }
 
-pack_address_block :: ModuleInfo -> AST.AddressBlock -> MyAddressBlock
-pack_address_block mi ab = MyAddressBlock {
+pack_address_block :: AST.AddressBlock -> MyAddressBlock
+pack_address_block ab = MyAddressBlock {
   domain = AST.Memory,
   addresses = SAST.addresses ab,
   properties = SAST.properties ab
@@ -232,6 +288,7 @@ generate_conj mi li = "TODO> NYI"
 
 
 -- generate forall with a explicit variable name
+{-
 forall_qual :: ModuleInfo -> String -> AST.NaturalSet -> [AST.Definition] -> String
 forall_qual mi varName ns body =
   "(" ++
@@ -246,7 +303,9 @@ forall_qual mi varName ns body =
      it_var = "IDT_" ++ varName
      it_list = "IDL_" ++ varName
      body_str = generate_conj (add_param mi it_var) body
+-}
 
+{-
 forall_uqr :: ModuleInfo -> AST.UnqualifiedRef -> String -> String
 forall_uqr mi ref body_str = case (AST.refIndex ref) of
   Nothing -> printf "(%s = %s, %s)" it_var id_var body_str
@@ -263,6 +322,7 @@ forall_uqr mi ref body_str = case (AST.refIndex ref) of
     it_var = "IDT_" ++ (AST.refName ref)
     itid_var = "IDI_" ++ (AST.refName ref)
     it_list = "IDL_" ++ (AST.refName ref)
+-}
 
 
 
@@ -289,12 +349,12 @@ gen_index uqr =
     gen_exp_simple (AST.Literal _ int) = show int
 
 
-gen_accept :: ModuleInfo -> AST.UnqualifiedRef -> [AST.AddressBlock] -> (Integer, [String]) -> (Integer, [String])
-gen_accept mi _ [] s = s
-gen_accept mi n (xs:x) (i, s) = gen_accept mi n x (i + 1, s ++ [acc])
+gen_accept :: AST.UnqualifiedRef -> [AST.AddressBlock] -> (Integer, [String]) -> (Integer, [String])
+gen_accept _ [] s = s
+gen_accept n (xs:x) (i, s) = gen_accept n x (i + 1, s ++ [acc])
     where
         acc = state_add_accept i (predicate "region" [generate n, generate pab, generate $ properties pab ])
-        pab = pack_address_block mi xs
+        pab = pack_address_block xs
 
 gen_base_address :: AST.Address -> String
 gen_base_address (AST.Address _ ws) = gen_single_ws $ ws !! 0
@@ -322,10 +382,12 @@ gen_blockoverlay src dst (om:x) (i, s) = gen_blockoverlay src dst  x (i + 1, s +
         trs = state_add_block_meta i src (show om) dst
 
 gen_body_defs :: ModuleInfo -> AST.Definition -> Integer -> (Integer, [String])
+instance PrologGenerator AST.Definition where
+    generate x = do
+
 gen_body_defs mi x i = case x of
-  (AST.Accepts _ n accepts) -> gen_accept mi n accepts (i, [])
-  --(1, [(assert 0 $ predicate "accept" [generate n, generate (new_ab acc)])
---    | acc <- accepts])
+  (AST.Accepts _ n accepts) -> do
+                        nStr <- generate n
   (AST.Maps _ _ _) -> gen_translate (map_spec_flatten mi x) (i, [])
    --(1, [(assert 0 $ predicate "translate"
     --[generate $ srcNode om, generate $ srcAddr om, generate $ targetNode om, generate $ targetAddr om])
@@ -337,10 +399,11 @@ gen_body_defs mi x i = case x of
         ([statevar i] ++ [gen_index ii] ++ (map generate args) ++ [statevar (i+1)]) ])
   -- (AST.Binds _ i binds) -> [forall_uqr mi i $ gen_bind_defs ("IDT_" ++ (AST.refName i)) binds]
   (AST.Binds _ ii binds) -> gen_bind_defs (gen_index ii) binds (i, [])
-  (AST.Forall _ varName varRange body) -> (0, [forall_qual mi varName varRange body])
+  --(AST.Forall _ varName varRange body) -> (0, [forall_qual mi varName varRange body])
+  (AST.Forall _ varName varRange body) -> throw $ NYIException "forall"
   (AST.Converts _ _ _ ) -> throw $ NYIException "Converts"
   where
-    new_ab ab = pack_address_block mi ab
+    new_ab ab = pack_address_block ab
 
 count_num_facts :: ModuleInfo -> AST.Definition -> Integer
 count_num_facts mi x = case x of
@@ -524,18 +587,17 @@ for_body params itvar (AST.NaturalSet _ ranges) body =
   where
     fbi = for_body_inner params itvar
 
-statevar :: Integer -> String
+statevar :: Int -> String
 statevar i = printf "S%i" i
 
---assert ::  Integer -> String -> String
---assert i x = "state_add" ++ parens ((statevar i) ++ ", " ++  x ++ ", " ++ (statevar (i + 1)))
+assert_translate ::  String -> String -> ModLevelStateS String
+assert_translate src dst = "assert_translate" ++ parens ((statevar i) ++ ", " ++  src ++ ", " ++ dst ++ ", " ++ (statevar (i + 1)))
 
-
-state_add_mapping ::  Integer -> String -> String -> String
-state_add_mapping i src dst = "assert_translate" ++ parens ((statevar i) ++ ", " ++  src ++ ", " ++ dst ++ ", " ++ (statevar (i + 1)))
-
-state_add_accept ::  Integer -> String -> String
-state_add_accept i reg = "assert_accept" ++ parens ((statevar i) ++ ", " ++  reg ++ ", " ++ (statevar (i + 1)))
+assert_accept ::  String -> ModLevelStateS String
+assert_accept reg = do
+    sIn <- get_state_var
+    sOut <- get_next_state_var
+    return $ "assert_accept" ++ parens (sIn ++ ", " ++  reg ++ ", " ++ sOut)
 
 state_add_overlay ::  Integer -> String -> String -> String
 state_add_overlay i src dst = "assert_overlay" ++ parens ((statevar i) ++ ", " ++  src ++ ", " ++ dst ++ ", " ++ (statevar (i + 1)))
